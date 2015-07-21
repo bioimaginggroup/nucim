61a3822e8e5f9b3bf9e367920c2ff60bd095f1f7
ua341fy <volker.schmid@stat.uni-muenchen.de>
2015-07-21 16:10:51 +0200

    find.spots: more flexibility

--- R/spots2.R
+++ R/spots2.R
@@ -7,7 +7,7 @@
 #' @return spot images in spot-color/, number of spots as txt files in spot-color/
 #' @export
 #'
-find.spots<-function(f,color,cores=1)
+find.spots<-function(f,color,thresh=1,thresh.auto=TRUE,cores=1)
 {
     orig<-getwd()
 setwd(f)
@@ -24,13 +24,13 @@ setwd(f)
     dir<-paste("spots-",color,sep="")
     if(length(list.files(dir))==0)dir.create(dir)
     
-    if(cores>1)jobs <- mclapply(files, find.spots.file, dir=dir,color=color)
-    if(cores==1)jobs <- lapply(files, find.spots.file, dir=dir,color=color)
+    if(cores>1)jobs <- mclapply(files, find.spots.file, dir=dir,color=color,thresh=thresh,thresh.auto=thresh.auto)
+    if(cores==1)jobs <- lapply(files, find.spots.file, dir=dir,color=color,thresh=thresh,thresh.auto=thresh.auto)
     
     setwd(orig)
 }
 
-find.spots.file<-function(file, dir,color)
+find.spots.file<-function(file, dir,color,thresh,thresh.auto)
     {
       try({
         print(file)
@@ -40,18 +40,24 @@ find.spots.file<-function(file, dir,color)
         
         col2<-col[mask==1]
         col2<-col2[col2!=0]
-        thresh<-c()
+        
+        if (thresh.auto){
+          
+        thresh.prop<-c()
         
         for (i in 100:200)
         {
           temp<-hist(col2,breaks=i,plot=FALSE)
           m<-min(which(diff(temp$counts)>0))
-          thresh<-c(thresh,temp$mids[m])
+          thresh.prop<-c(thresh.prop,temp$mids[m])
         }
         gc()
         
-        thresh<-quantile(thresh,na.rm=TRUE,probs=2/3)
+        thresh.prop<-quantile(thresh.prop,na.rm=TRUE,probs=1/2)
         #thresh<-max(thresh,na.rm=TRUE,2/3)
+        thresh=thresh*thresh.prop
+        }
+        
         white<-array(ifelse(col>thresh,1,0)*mask,dim(col))
         spots<-bwlabel3d(white)
         #spots<-aperm(spots,c(2,1,3))
