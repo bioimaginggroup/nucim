00aa62414abf7e25c83cbc37b996a5cfc554e3e8
volkerschmid <volker.schmid@stat.uni-muenchen.de>
2016-09-23 17:29:24 +0200

    class.neigbours.folder(): removed bug

--- R/class_neighbours.R
+++ R/class_neighbours.R
@@ -35,7 +35,7 @@ class.neighbours.folder<-function(inputfolder,outputfolder, N=7)
   files<-list.files(inputfolder)
   for (i in files)
   {
-    img<-readTIF(paste0(inputfolder,i,sep="/"))
+    img<-bioimagetools::readTIF(paste(inputfolder,i,sep="/"))
     img<-round(img*7,0)
     temp<-class.neighbours(img,1:N,N.max=N)
     for (j in 1:N)
