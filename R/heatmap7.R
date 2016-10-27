e4861cdde03daf024beed65c53f73a88c130348c
volkerschmid <volker.schmid@stat.uni-muenchen.de>
2016-10-27 16:34:09 +0200

    +heatmap7()

---
+++ R/heatmap7.R
@@ -0,0 +1,7 @@
+#' Heatmap colors for 7 classes
+#'
+#' @param ... parameters are ignored.
+#' @export
+#'
+#' @examples barplot(7:1,col=heatmap7())
+heatmap7=function(...){return(c("#3d00b2","#a200b5","#774448","#ff7b00","#ffbd00","#fff932","#ffffff"))}
