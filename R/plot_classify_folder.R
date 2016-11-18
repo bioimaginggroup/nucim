437db6f5a921f331d00eafd59a6ceb176e978df4
Volker Schmid <volker.schmid@lmu.de>
2016-11-18 12:50:00 +0100

    plot_classify.folder() +method option

--- R/plot_classify_folder.R
+++ R/plot_classify_folder.R
@@ -4,12 +4,13 @@
 #' @param N number of classes, default: 7
 #' @param col color of bars, either one or a vector of hex RGB characters 
 #' @param cores number of cores to use in parallel (needs parallel package if cores>1)
+#' @param method method for error bars ("sd", "minmax", "quartile")
 #'
 #' @return plots
 #' @importFrom grDevices grey
 #' @import utils
 #' @export
-plot_classify.folder<-function(path, N=7, cores=1 ,col=grDevices::grey(0.7))
+plot_classify.folder<-function(path, N=7, cores=1 ,col=grDevices::grey(0.7),method="sd")
 {
   orig<-getwd()
   setwd(path)
@@ -20,7 +21,9 @@ plot_classify.folder<-function(path, N=7, cores=1 ,col=grDevices::grey(0.7))
   if(cores>1)tables <- parallel::mclapply(files, read.table.helper, N=N, mc.cores=cores)
   if(cores>1)tables <- lapply(files, read.table.helper, N=N)
   tables<-array(unlist(tables),c(N,length(tables)))
-  barplot_with_interval(tables,xlab="chromatin compaction levels",ylab="percentage",col=col)
+  qu=NULL
+  if (method=="quartile"){method="quantile";qu=c(.25,.75)}
+  barplot_with_interval(tables,xlab="chromatin compaction levels",ylab="percentage",col=col,method=method,qu=qu)
 }
 
 read.table.helper<-function(file, N){
